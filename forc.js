736bf6d63718a04601f8eeffeebba021549eb594
hulyadogan <hullyadoggan@gmail.com>
2016-03-18 10:30:31 +0200

    Added files via upload

---
+++ forc.js
@@ -0,0 +1,19 @@
+
+var elemID ="#disp";/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+$().ready(function(){
+    for(var i=0; i<10; i++) {
+        top= Math.floor((Math.random()*70px)
+        $(elemID).append (
+        "<div id='" + i + "' class='appearence' style='top: " 
+        + top +"px; left:" + left + "px;'> + i + "</div>");
+
+                
+                
+                
+    }
+}
+
