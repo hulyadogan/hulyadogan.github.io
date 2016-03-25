5df871ed1ba917d079f9ec573ccf4bb6e4c2cd4e
hulyadogan <hullyadoggan@gmail.com>
2016-03-25 10:29:28 +0200

    Added files via upload

---
+++ course.js
@@ -0,0 +1,28 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+var course={
+    'code':'304',
+    'teachername':'aytenyılmaz',
+    'dayofcourse':'monday',
+    'topic':'neuroscience'[
+        {'courseID':'' ,
+          'grade':'85' 
+      }
+            
+    ]
+};
+function Course(code,teacher,day,topic){
+       this.code:code;
+       this.teacher:teachername;
+       this.day:day;
+       this.topic:topic;
+       this.getCourseCode=function(){
+           return this.code+this.topic;
+       };
+       
+};
+var c1=
+var c2=
---
+++ student.js
@@ -0,0 +1,35 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.KONU:objectdefinition
+ */
+
+
+var s1=student={
+    'ID'
+}
+
+var student={
+    'ID':'',
+    ''
+}
+        calcGPA:function(){
+        var gpa=0;
+        for(var i=0,i<this.courses.length;i++){
+            gpa
+        }
+        };
+       
+        
+        var s1=student;
+        s1['ID']=;
+        s1[''];
+        for(var i=0; i<5;i++) {
+            s1['courses'][i]={'courseID':'c'+i, 'grade';Math.random()*100+1};
+        }
+        
+        
+        
+        function Person(name, surname,age,gender){
+            this.firstName=
+        }
